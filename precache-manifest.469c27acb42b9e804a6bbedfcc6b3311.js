b1378aa05516b34a8270d29949c8fedc362940e9
manishgupta248 <manishgupta248@gmail.com>
2023-01-22 17:29:48 +0530

    Updates

---
+++ precache-manifest.469c27acb42b9e804a6bbedfcc6b3311.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6d5bd421d434c72fccf6",
+    "url": "/man-textutils/static/css/main.b2b58920.chunk.css"
+  },
+  {
+    "revision": "6d5bd421d434c72fccf6",
+    "url": "/man-textutils/static/js/main.6d5bd421.chunk.js"
+  },
+  {
+    "revision": "8c8ed3e6ce86a909c145",
+    "url": "/man-textutils/static/js/1.8c8ed3e6.chunk.js"
+  },
+  {
+    "revision": "2b8e73089cc5c607c393",
+    "url": "/man-textutils/static/js/2.2b8e7308.chunk.js"
+  },
+  {
+    "revision": "34bb93e1854a7d1a1847",
+    "url": "/man-textutils/static/js/runtime~main.34bb93e1.js"
+  },
+  {
+    "revision": "a88ec5a88102815ea8858beae134816f",
+    "url": "/man-textutils/index.html"
+  }
+];
